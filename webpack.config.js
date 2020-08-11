48510d60368d128b06141fbcb8da361a6ef499b3
vanmer <60948275+vanmer@users.noreply.github.com>
2020-08-11 16:00:24 +0200

    configured webpack to use contentHash in bundle file name; install HtmlWebpackPlugin to generate a new index.html; it automatically includes the correct script tag at the bottom; create template file ; delete index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+      template: "./src/template.html"
+      })
+    ],
     module: {
         rules: [
           {
